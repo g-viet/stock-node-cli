ec23c77ee8329125323d3a25cf47501f33b7adbf
Viet Nguyen <vietbktn@gmail.com>
2019-04-26 18:21:10 +0900

    Refactor commander

--- src/index.ts
+++ src/index.ts
@@ -5,37 +5,29 @@ import * as StockApi from './stock_api';
 
 program
     .version('0.0.1')
-    .description("Stock CLI for getting stock information")
-    .arguments('get <stock> [otherStocks...]', 'get one or more stocks price')
-    .action((cmd: string, stocks: string[] = []) => {
-        switch(cmd) {
-            case "gets":
-                if (stocks.length == 0) {
-                    console.log("You must include stock codes!! For example: ");
-                    console.log("`stocknode gets VCB TCM`");
-                    return;
-                }
-                StockApi.gets(stocks);
-                break;
-            case "stream":
-                if (stocks.length == 0) {
-                    console.log("You must include stock code!! For example: ");
-                    console.log("`stocknode stream VCB`");
-                    return;
-                }
-                StockApi.stream(stocks[0]);
-                break;
-            case "group":
-                if (stocks.length == 0) {
-                    console.log("You must include industry group!! For example: ");
-                    console.log("`stocknode group INDUSTRY_GROUP [OPTIONS]`");
-                    return;
-                }
-                StockApi.group(stocks[0])
-                break;
-            default:
-                console.log(`No "${cmd}" method.`)
-        }
-    });
+    .description("Stock CLI for getting stock information");
 
+program
+    .command('gets <stocks...>')
+    .description('get one or more stocks price')
+    .action((stocks: string[]) => {
+        if (stocks.length == 0) {
+            console.log("You must include stock codes!! For example: ");
+            console.log("`stocknode gets VCB TCM`");
+            return;
+        }
+        StockApi.gets(stocks);
+    });
+program
+    .command('stream <stockCode>')
+    .description('get streaming stock price')
+    .action((stockCode: string) => {
+        StockApi.stream(stockCode);
+    });
+program
+    .command('group [groupCode]')
+    .description('get stock prices of industry group')
+    .action((groupCode: string) => {
+        StockApi.group(groupCode);
+    });
 program.parse(process.argv);
